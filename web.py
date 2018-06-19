06dbccb89d44c4a467f55920b3417d0c0317b72e
Saxon Royalist <theroguelordbear@gmail.com>
2018-06-19 16:20:32 +1000

    Mainly changes to the way that the submission form works. Enjoy working with this pile of trash.

--- web.py
+++ web.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     if not session.get('user_id'):
-        return render_template('loginuser.html')
+        return render_template('homepage.html')
     else:
         return render_template('user.html')
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
 # Read username and password from form parameters.
@@ -58,21 +59,42 @@ def do_admin_login():
     cursor.close()
     cnx.close()
     return render_template('user.html', userid=session.get("user_id"), name=session.get("user_1name") )
-@app.route("/createuser")
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/createuser", methods=['POST'])
 def createuser():
-    return render_template('creation.html')
+    username = str(request.form['username'])
+    password = str(request.form['password'])
+    email = str(request.form['email'])
+    firstname = str(request.form['firstname'])
+    lastname = str(request.form['lastname'])
+   
+    cnx = mysql.connector.connect(user='root', database='fakebank')
+    cursor = cnx.cursor()
+    sql = "INSERT INTO fb_userlogin (user_login, user_pass, user_email, user_firstname, user_lastname) VALUES (%s, %s, %s, %s, %s)"
+    val = (username, password, email, firstname, lastname)
+    cursor.execute(sql, val)
+    success = (cursor.rowsaffected != 0)
+    #cnx.commit()
+    #print(cursor.rowsaffected, "record inserted.")
+    cursor.close()
+    cnx.close()
+    if success == True:
+        createsuccess()
+    else:
+        error()
 
 @app.route("/createsuccess")
 def createsuccess():
     return render_template('createsuccess.html')
-    a = data()
-    cnx = mysql.connector.connect(user='root', database='fakebank')
-    cursor = cnx.cursor()
-    query = ("INSERT INTO fb_userlogin (user_login, user_email, user_pass, user_firstname, user_lastname, user_registertime, user_active) VALUES (\'" + a.usernamen + "\',\'" + a.passwordn + "\', \'" + a.emailn + "\,'\'" + a.firstnamen + "\',\'" + a.lastnamen + "\',\'" + a.datetimeinput + "\',\'" + a.activeaccount + "\')") 
-    cursor.execute(query) 
-    cnx.commit()
-    cursor.close()
-    cnx.close()
+
+@app.route("/error")
+def error():
+    return render_template('error.html')
 
 
 @app.route("/logout")
